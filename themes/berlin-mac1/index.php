e0bf326dc8e715ccc0913fc086de2436d155b484
Greg Lawrence <gvlawrence@gmail.com>
2016-02-08 06:57:22 +0000

    recommit after pull

--- themes/berlin-mac1/index.php
+++ themes/berlin-mac1/index.php
@@ -6,31 +6,9 @@
     <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
     <p><?php echo $homepageText; ?></p>
     <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Collection')): ?>
-    <!-- Featured Collection -->
-    <div id="featured-collection" class="featured">
-        <h2><?php echo __('Featured Collection'); ?></h2>
-        <?php echo random_featured_collection(); ?>
-    </div><!-- end featured collection -->
-    <?php endif; ?>
     <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
     <!-- Featured Exhibit -->
     <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-    <?php endif; ?>
-
-</div><!-- end primary -->
-
-<div id="secondary">
-    <h2>&nbsp;Opening Hours</h2>
-    <h4>&nbsp;&nbsp;9.30am - 4.30pm &nbsp;&nbsp;Monday - Friday</h4>
-    <p>&nbsp;&nbsp;(Other times by special arrangement)</p>
-
-    <?php if (get_theme_option('Display Featured Item') == 1): ?>
-    <!-- Featured Item -->
-    <div id="featured-item" class="featured">
-        <h2><?php echo __('Featured Item'); ?></h2>
-        <?php echo random_featured_items(1); ?>
-    </div><!--end featured-item-->
     <?php endif; ?>
 
     <?php
@@ -48,7 +26,26 @@
         <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
     </div><!--end recent-items -->
     <?php endif; ?>
-    
+
+</div><!-- end primary -->
+
+<div id="secondary">
+    <?php if (get_theme_option('Display Featured Collection')): ?>
+    <!-- Featured Collection -->
+    <div id="featured-collection" class="featured">
+        <h2><?php echo __('Featured Collection'); ?></h2>
+        <?php echo random_featured_collection(); ?>
+    </div><!-- end featured collection -->
+    <?php endif; ?>
+
+    <?php if (get_theme_option('Display Featured Item') == 1): ?>
+    <!-- Featured Item -->
+    <div id="featured-item" class="featured">
+        <h2><?php echo __('Featured Item'); ?></h2>
+        <?php echo random_featured_items(1); ?>
+    </div><!--end featured-item-->
+    <?php endif; ?>
+
     <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
 
 </div><!-- end secondary -->
